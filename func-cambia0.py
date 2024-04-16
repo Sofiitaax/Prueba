f7fe1a34149b7a1fd4a68e41e7a8c3760bc4d74e
Guillermo <guillermosdlg@gmail.com>
2024-04-16 08:12:07 -0600

    Ya terminé la función.

---
+++ func-cambia0.py
@@ -0,0 +1,14 @@
+def cambia0(num):
+    if isinstance(num, int):
+        return cambia0Aux(num, 0)
+    else:
+        return
+
+def cambia0Aux(num, exp):
+    if num == 0:
+        return 0
+    elif num % 10 == 0:
+        return 1 * 10 ** exp + cambia0Aux(num//10, exp + 1)
+    else:
+        return num%10*10**exp + cambia0Aux(num//10, exp + 1)
+
